9a40620351135590ff1eaca8b744d34a07e9e219
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2015-07-14 14:46:38 +0300

    sidekiq enabled

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
 	
 	mount_uploader :track, TrackUploader
-	#store_in_background :track
+	store_in_background :track
 	self.primary_key = :aid
 	has_many :chunks, -> { order('part_number') }, dependent: :destroy, :foreign_key => :aid
 
